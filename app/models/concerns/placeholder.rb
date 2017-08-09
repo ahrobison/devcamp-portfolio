20186e59d59ab13c0c8e92995c55c7c2c6a2e2e8
ahrobison <ahrob27@gmail.com>
2017-08-08 23:37:06 -0600

    Updated file upload forms and refactored placeholder functionality into view

--- app/models/concerns/placeholder.rb
+++
@@ -1,9 +0,0 @@
-module Placeholder
-	extend ActiveSupport::Concern
-
-	def self.image_generator(height:, width:)
-		def set_defaults
- 			"http://via.placeholder.it/#{height}x#{width}"
- 		end
-  	end
-end
